9746fbd069700b0c6199033ebe02d6099856b6b1
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-05 21:19:10 +0000

    Function basic definition

---
+++ calculateSalesTax.js
@@ -0,0 +1,19 @@
+
+function calculateSalesTax(salesData, taxRates) {
+  // Implement your code here
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
