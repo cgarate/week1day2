c001527b9cf96cdcb062eb8afb75e0af11005460
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-05 21:20:07 +0000

    Added test data

--- calculateSalesTax.js
+++ calculateSalesTax.js
@@ -1,7 +1,31 @@
 
 function calculateSalesTax(salesData, taxRates) {
-  // Implement your code here
+
 }
+
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
 
 var results = calculateSalesTax(companySalesData, salesTaxRates);
 
