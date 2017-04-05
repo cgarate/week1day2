e2c07b45a040e79ea3715e093c01cec9c93762b9
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-05 20:53:41 +0000

    Finished function with second loop where the indices are being inserted in the empty arrays created earlier. Added drive tests and an extra key with the orig string

--- getPosition.js
+++ getPosition.js
@@ -12,23 +12,27 @@ V1.0
 
 function getPosition(str) {
 
-  var stringStats = {};
+  var stringStats = { originalString : str};
 
   // We make a first run through the string and take the letters in it and initialize the object with an empty array.
   for (var i = 0; i < str.length; i++) {
     // Found a way to create a dynamic key with brackets ---> obj[variableReference]
     stringStats[str[i]] = [];
   }
+  // Once we have the object initialized with empty arrays we can loop again to finish the count.
+  for (i = 0; i < str.length; i++) {
+    stringStats[str[i]] = stringStats[str[i]].concat(i);
+  }
 
   return stringStats;
 }
 
 console.log(getPosition("lighthouse in the house"));
-/*console.log(getPosition("Lorem Ipsum whatevaaaaaaaaaaaa!!!"));
+console.log(getPosition("Lorem Ipsum whatevaaaaaaaaaaaa!!!"));
 console.log(getPosition("Supercalifragilisticoespialidoso."));
 console.log(getPosition("The big brown fox jumped over the lazy dog."));
 console.log(getPosition("\{\}|:\"<>?\"\}"));
 console.log(getPosition("~!@#$%^&*()_+"));
 console.log(getPosition("`1234567890-="));
 // Escaped the backslash
-console.log(getPosition("qwertyuiop[][\\asdfghjkl;'zxcvbnm,./"));*/
+console.log(getPosition("qwertyuiop[][\\asdfghjkl;'zxcvbnm,./"));
