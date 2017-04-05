0949af3223097fa04ae58ec0c2875d24edd66ddf
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-05 19:31:45 +0000

    Finished the function. Added the 2nd loop to add up the occurrences and added some drive tests at the bottom.

--- countletters.js
+++ countletters.js
@@ -1,3 +1,5 @@
+
+// countLetters()
 // count unique characters and their frequency in the string passed.
 // return an object with keys represented by each of the letters found and the number of times found as value.
 
@@ -15,11 +17,25 @@ function countLetters(str) {
 
   // We make a first run through the string and take the letters in it and initialize the object with a 0 count.
   for (var i = 0; i < noSpaces.length; i++) {
-    // Found a way to create a dynamic key.
+    // Found a way to create a dynamic key with brackets ---> obj[variableReference]
     stringStats[noSpaces[i]] = 0;
+  }
+
+  // Once we have the object initialized with zeros we can loop again to finish the count.
+  for (i = 0; i < noSpaces.length; i++) {
+    stringStats[noSpaces[i]] += 1;
   }
 
   return stringStats;
 }
 
+console.log(countLetters("Lorem Ipsum whatevaaaaaaaaaaaa!!!"));
+console.log(countLetters("Supercalifragilisticoespialidoso."));
 console.log(countLetters("The big brown fox jumped over the lazy dog."));
+// As I suspected special characters need to be escaped or else the function explodes.
+// The example below brings up another weird effect. Run it to see.
+console.log(countLetters("\{\}|:\"<>?\"\}"));
+console.log(countLetters("~!@#$%^&*()_+"));
+console.log(countLetters("`1234567890-="));
+// Escaped the backslash
+console.log(countLetters("qwertyuiop[][\\asdfghjkl;'zxcvbnm,./"));
